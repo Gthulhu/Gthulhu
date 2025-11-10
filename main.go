346905dc23dbb80a877e32e6c46f8dc9ff3a7884
Ian Chen <iancodinghtml@gmail.com>
2025-11-11 06:10:27 +0800

    feat: adjust vtime based on execution time

--- main.go
+++ main.go
@@ -196,7 +196,7 @@ func main() {
 
 			// Evaluate used task time slice.
 			nrWaiting := core.GetNrQueued() + core.GetNrScheduled() + 1
-			task.Vtime = t.Vtime
+			task.Vtime = t.Vtime + min(t.SumExecRuntime, SLICE_NS_DEFAULT*100)
 
 			// Check if a custom execution time was set by a scheduling strategy
 			customTime := bpfModule.DetermineTimeSlice(t)
